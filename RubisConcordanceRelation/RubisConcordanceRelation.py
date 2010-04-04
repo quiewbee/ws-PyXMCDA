d96536538efd6fe96fceb0276f749323f7c3f8ec
Thomas Veneziano <quiewbee@gmail.com>
2010-04-04 12:50:37 +0200

    Modification of the code of RubisConcordanceRelation.py and weightedSum.py for a better robustness

--- RubisConcordanceRelation/RubisConcordanceRelation.py
+++ RubisConcordanceRelation/RubisConcordanceRelation.py
@@ -3,13 +3,13 @@ import sys
 import getopt
 import subprocess
 
-
-
 import PyXMCDA
 
 from optparse import OptionParser
 
+
 ###
+
 
 parser = OptionParser()
 
@@ -21,65 +21,58 @@ parser.add_option("-o", "--out", dest="out_dir")
 in_dir = options.in_dir
 out_dir = options.out_dir
 
-XMCDA_NS = "http://www.decision-deck.org/2009/XMCDA-2.0.0"
-
-# on cree le fichier pour les logs
-fileMessages = open(out_dir+"/messages.xml", 'w')
-PyXMCDA.writeHeader (fileMessages)
-
+# Creating a list for error messages
 errorList = []
 
-status = 0
-
+# If some mandatory input files are missing
 if not os.path.isfile (in_dir+"/alternatives.xml") or not os.path.isfile (in_dir+"/criteria.xml") or not os.path.isfile (in_dir+"/criteriaWeights.xml") or not os.path.isfile (in_dir+"/performanceTable.xml") :
-	status = 1
 	errorList.append("Some input files are missing")
 
 else :
 	
+	# We parse all the mandatory input files
 	xmltree_alternatives = PyXMCDA.parseValidate(in_dir+"/alternatives.xml")
 	xmltree_criteria = PyXMCDA.parseValidate(in_dir+"/criteria.xml")
 	xmltree_weights = PyXMCDA.parseValidate(in_dir+"/criteriaWeights.xml")
 	xmltree_perfTable = PyXMCDA.parseValidate(in_dir+"/performanceTable.xml")
 	
+	# We check if all madatory input files are valid
 	if xmltree_alternatives == None :
-		status = 1
 		errorList.append("The alternatives file can't be validated.")
 	if xmltree_criteria == None :
-		status = 1
 		errorList.append("The criteria file can't be validated.")
 	if xmltree_perfTable == None :
-		status = 1
 		errorList.append("The performance table file can't be validated.")
 	if xmltree_weights == None :
-		status = 1
 		errorList.append("The criteria weights file can't be validated.")
+	
+	# By default, the valuation domain is fixed to [0,1]	
 	minValDomain = 0
 	maxValDomain = 1
+	
+	# If a valuation domain input file has been provided
 	if os.path.isfile (in_dir+"/valuationDomain.xml") :
+	
 		xmltree_valuation = PyXMCDA.parseValidate(in_dir+"/valuationDomain.xml")
 		if xmltree_valuation == None :
-			status = 1
 			errorList.append ("valuationDomain file can't be validated.")
-		else :
-			mini = PyXMCDA.getParameterByName (xmltree_valuation, "min", "valuationDomain")
-			maxi = PyXMCDA.getParameterByName (xmltree_valuation, "max", "valuationDomain")
 			
-			if not isinstance(mini,float) and not isinstance(mini,int) :
+		else :
+		
+			minValDomain = PyXMCDA.getParameterByName (xmltree_valuation, "min", "valuationDomain")
+			maxValDomain = PyXMCDA.getParameterByName (xmltree_valuation, "max", "valuationDomain")
+			
+			# We check the validity of the parameters
+			if not isinstance(minValDomain,float) and not isinstance(minValDomain,int) :
 				errorList.append ("min value should be an integer or a real")
-				status = 1
-			if not isinstance(mini,float) and not isinstance(mini,int) :
+			if not isinstance(maxValDomain,float) and not isinstance(maxValDomain,int) :
 				errorList.append ("max value should be an integer or a real")
-				status = 1
-			if status !=1 :
-				if mini >= maxi :
+				
+			if not errorList :
+				if minValDomain >= maxValDomain :
 					errorList.append ("The max value should be strictly greater than the min value")
-					status = 1
-			if status != 1 :
-				minValDomain = mini
-				maxValDomain = maxi
 
-	if status != 1 :
+	if not errorList :
 	
 		alternativesId = PyXMCDA.getAlternativesID(xmltree_alternatives)
 		criteriaId = PyXMCDA.getCriteriaID(xmltree_criteria)
@@ -87,32 +80,29 @@ else :
 		thresholds = PyXMCDA.getConstantThresholds (xmltree_criteria, criteriaId)
 		weights = PyXMCDA.getCriterionValue (xmltree_weights, criteriaId)
 	
-		if alternativesId == [] :
-			status = 1
+		if not alternativesId :
 			errorList.append("No alternatives found. Is your alternatives file correct ?")
-		if criteriaId == [] :
-			status = 1
+		if not criteriaId :
 			errorList.append("No criteria found. Is your criteria file correct ?")
-		if perfTable == [] :
-			status = 1
+		if not perfTable :
 			errorList.append("No performance table found. Is your performance table file correct ?")
-		if weights == [] :
-			status = 1
+		if not weights :
 			errorList.append("No weights found. Is your weights file correct ?")
-				
-if status == 1 :
-	# Il y a eu des erreurs, on arrete
-	PyXMCDA.write_error_messages (fileMessages, errorList)
 	
-else :
+if not errorList :
 
-	#On calcule la somme des poids
+	# We compute the weight sum (only the weights associated to active criteria)
 	sumWeights = 0.0
 	
-	for wei in weights.keys() :
-		sumWeights = sumWeights + weights[wei]
+	for crit in criteriaId :
+		try :
+			sumWeights = sumWeights + weights[crit]
+		except :
+			errorList.append("There is no defined weight for criterion "+crit+".")
 
-	#On genere le fichier resultat
+if not errorList :
+
+	# We compute the alternative comparisons values
 	fileAltValues = open(out_dir+"/alternativesComparisons.xml", 'w')
 	PyXMCDA.writeHeader (fileAltValues)
 	
@@ -120,48 +110,68 @@ else :
 		
 	for alt1 in alternativesId :
 		for alt2 in alternativesId :
+		
 			fileAltValues.write("<pair><initial><alternativeID>"+alt1+"</alternativeID></initial><terminal><alternativeID>"+alt2+"</alternativeID></terminal>")
-			posCrit = -1
+			
 			sum = 0.0
+			probComparisons = False
+			
 			for crit in criteriaId :
-				
-				if perfTable[alt1][crit] >= perfTable[alt2][crit] :
-					sum = sum + maxValDomain*weights[crit]
-				else :
-					if not thresholds[crit].has_key('indifference') and not thresholds[crit].has_key('preference') :
-						# aucun seuil, indif ou pref, defini
-						sum = sum + minValDomain*weights[crit]
+			
+				try :
+					if perfTable[alt1][crit] >= perfTable[alt2][crit] :
+						sum = sum + maxValDomain*weights[crit]
 					else :
-						if (thresholds[crit].has_key('indifference') != thresholds[crit].has_key('preference')) :
-							#un seuil, indif ou pref, est defini
-							if thresholds[crit].has_key('indifference') :
+						if not thresholds[crit].has_key('indifference') and not thresholds[crit].has_key('preference') :
+							# No constant thresholds are defined for the seleccted criterion
+							sum = sum + minValDomain*weights[crit]
+						else :
+							if (thresholds[crit].has_key('indifference') != thresholds[crit].has_key('preference')) :
+								# An indifference Xor a preference threshold has been defined
+								if thresholds[crit].has_key('indifference') :
+									if perfTable[alt1][crit] + thresholds[crit]["indifference"] >= perfTable[alt2][crit] :
+										sum = sum + maxValDomain*weights[crit]
+									else :
+										sum = sum + minValDomain*weights[crit]
+								else :
+									if perfTable[alt1][crit] + thresholds[crit]["preference"] >= perfTable[alt2][crit] :
+										sum = sum + maxValDomain*weights[crit]
+									else :
+										sum = sum + minValDomain*weights[crit]
+							else :
+								# An indifference and a preference thresholds have been defined
 								if perfTable[alt1][crit] + thresholds[crit]["indifference"] >= perfTable[alt2][crit] :
 									sum = sum + maxValDomain*weights[crit]
+								elif perfTable[alt1][crit] + thresholds[crit]["preference"] >= perfTable[alt2][crit] :
+									sum = sum + (float(maxValDomain+minValDomain))*weights[crit]/2.0
 								else :
 									sum = sum + minValDomain*weights[crit]
-							else :
-								if perfTable[alt1][crit] + thresholds[crit]["preference"] >= perfTable[alt2][crit] :
-									sum = sum + maxValDomain*weights[crit]
-								else :
-									sum = sum + minValDomain*weights[crit]
-						else :
-							# il y a deux seuils
-							if perfTable[alt1][crit] + thresholds[crit]["indifference"] >= perfTable[alt2][crit] :
-								sum = sum + maxValDomain*weights[crit]
-							elif perfTable[alt1][crit] + thresholds[crit]["preference"] >= perfTable[alt2][crit] :
-								sum = sum + (float(maxValDomain+minValDomain))*weights[crit]/2.0
-							else :
-								sum = sum + minValDomain*weights[crit]
-			sum = sum/sumWeights				
-			fileAltValues.write ("<value><real>"+str(sum)+"</real></value></pair>\n")
+				except :
+					errorList.append("some alternatives evaluations are missing for criterion "+crit+", during the comparison between "+alt1+" and "+alt2+".")
+					probComparisons = True
+			
+			if not probComparisons :
+				sum = sum/sumWeights				
+				fileAltValues.write ("<value><real>"+str(sum)+"</real></value></pair>\n")
+			else :
+				fileAltValues.write ("<value><NA>not available</NA></value></pair>\n")
 		
 	fileAltValues.write ("</pairs></alternativesComparisons>")
 		
 	PyXMCDA.writeFooter(fileAltValues)
 	fileAltValues.close()
 
+# Creating log and error file, messages.xml
+fileMessages = open(out_dir+"/messages.xml", 'w')
+PyXMCDA.writeHeader (fileMessages)
 
+if not errorList :
 
 	PyXMCDA.writeLogMessages (fileMessages, ["Execution ok"])
-	PyXMCDA.writeFooter(fileMessages)
+else :
+	PyXMCDA.writeErrorMessages (fileMessages, errorList)
+	
+PyXMCDA.writeFooter(fileMessages)
+fileMessages.close()
+	
 
--- weightedSum/weightedSum.py
+++ weightedSum/weightedSum.py
@@ -7,7 +7,9 @@ import PyXMCDA
 
 from optparse import OptionParser
 
+
 ###
+
 
 parser = OptionParser()
 
@@ -19,89 +21,98 @@ parser.add_option("-o", "--out", dest="out_dir")
 in_dir = options.in_dir
 out_dir = options.out_dir
 
-XMCDA_NS = "http://www.decision-deck.org/2009/XMCDA-2.0.0"
-
-# on cree le fichier pour les logs
-fileMessages = open(out_dir+"/messages.xml", 'w')
-PyXMCDA.writeHeader (fileMessages)
-
+# Creating a list for error messages
 errorList = []
 
-status = 0
-
-if not os.path.isfile (in_dir+"/alternatives.xml") or not os.path.isfile (in_dir+"/criteria.xml") or not os.path.isfile (in_dir+"/performanceTable.xml") or not os.path.isfile (in_dir+"/criteriaWeights.xml") :
-	status = 1
+# If some mandatory input files are missing
+if not os.path.isfile (in_dir+"/alternatives.xml") or not os.path.isfile (in_dir+"/criteria.xml") or not os.path.isfile (in_dir+"/criteriaWeights.xml") or not os.path.isfile (in_dir+"/performanceTable.xml") :
 	errorList.append("Some input files are missing")
 
 else :
-
+	
+	# We parse all the mandatory input files
 	xmltree_alternatives = PyXMCDA.parseValidate(in_dir+"/alternatives.xml")
 	xmltree_criteria = PyXMCDA.parseValidate(in_dir+"/criteria.xml")
-	xmltree_perfTable = PyXMCDA.parseValidate(in_dir+"/performanceTable.xml")
 	xmltree_weights = PyXMCDA.parseValidate(in_dir+"/criteriaWeights.xml")
+	xmltree_perfTable = PyXMCDA.parseValidate(in_dir+"/performanceTable.xml")
 	
+	# We check if all madatory input files are valid
 	if xmltree_alternatives == None :
-		status = 1
 		errorList.append("The alternatives file can't be validated.")
 	if xmltree_criteria == None :
-		status = 1
 		errorList.append("The criteria file can't be validated.")
 	if xmltree_perfTable == None :
-		status = 1
 		errorList.append("The performance table file can't be validated.")
 	if xmltree_weights == None :
-		status = 1
-		errorList.append("The criteriaWeights file can't be validated.")
-
-	if status != 1 :
+		errorList.append("The criteria weights file can't be validated.")
+	
+	if not errorList :
 	
 		alternativesId = PyXMCDA.getAlternativesID(xmltree_alternatives)
 		criteriaId = PyXMCDA.getCriteriaID(xmltree_criteria)
 		perfTable = PyXMCDA.getPerformanceTable(xmltree_perfTable, alternativesId, criteriaId)
+		thresholds = PyXMCDA.getConstantThresholds (xmltree_criteria, criteriaId)
 		weights = PyXMCDA.getCriterionValue (xmltree_weights, criteriaId)
 	
-		if alternativesId == [] :
-			status = 1
+		if not alternativesId :
 			errorList.append("No alternatives found. Is your alternatives file correct ?")
-		if criteriaId == [] :
-			status = 1
+		if not criteriaId :
 			errorList.append("No criteria found. Is your criteria file correct ?")
-		if perfTable == [] :
-			status = 1
+		if not perfTable :
 			errorList.append("No performance table found. Is your performance table file correct ?")
-		if weights == [] :
-			status = 1
+		if not weights :
 			errorList.append("No weights found. Is your weights file correct ?")
 
-if status == 1 :
-	# Il y a eu des erreurs, on arrete
-	PyXMCDA.write_error_messages (fileMessages, errorList)
-	
-else :
 
-	#On genere le fichier resultat
+if not errorList :
+
+	# We check if a criterion weight is defined for all the active criteria
+	for crit in criteriaId :
+		if not weights.has_key(crit) :
+			errorList.append("There is no defined weight for criterion "+crit+".")
+	
+	
+if not errorList :
+	
+	# We compute the weighted sum for all the active alternatives
 	fileAltValues = open(out_dir+"/alternativesValues.xml", 'w')
 	PyXMCDA.writeHeader (fileAltValues)
 	
 	fileAltValues.write ("<alternativesValues>\n")
 	
 	for alt in alternativesId :
-		fileAltValues.write("<alternativeValue><alternativeID>"+alt+"</alternativeID><value><real>")
-		sum = 0
-		
+	
+		fileAltValues.write("<alternativeValue><alternativeID>"+alt+"</alternativeID><value>")
+		sum = 0.0
+		errorAlt = False
+
 		for crit in criteriaId :
-			sum = sum + weights[crit] * perfTable[alt][crit]
-		fileAltValues.write (str(sum))
-		fileAltValues.write("</real></value></alternativeValue>\n")
+			
+			try :
+				sum = sum + weights[crit] * perfTable[alt][crit]
+			except :
+				errorList.append("No evaluation found for alternative "+alt+" on criterion "+crit+" in the performance table.")
+				errorAlt = True
+	
+		if not errorAlt :	
+			fileAltValues.write ("<real>"+str(sum)+"</real></value></alternativeValue>\n")
+		else :
+			fileAltValues.write ("<NA>not available</NA></value></alternativeValue>\n")
 		
 	fileAltValues.write ("</alternativesValues>")
 	
 	PyXMCDA.writeFooter(fileAltValues)
 	fileAltValues.close()
 
+# Creating log and error file, messages.xml
+fileMessages = open(out_dir+"/messages.xml", 'w')
+PyXMCDA.writeHeader (fileMessages)
 
+if not errorList :
 
 	PyXMCDA.writeLogMessages (fileMessages, ["Execution ok"])
-	PyXMCDA.writeFooter(fileMessages)
-
+else :
+	PyXMCDA.writeErrorMessages (fileMessages, errorList)
+	
+PyXMCDA.writeFooter(fileMessages)
 fileMessages.close()

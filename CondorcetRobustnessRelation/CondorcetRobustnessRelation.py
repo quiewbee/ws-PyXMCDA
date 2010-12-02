bfdd57c78970159168b2b183bc79e014ee0615cf
Thomas Veneziano <quiewbee@gmail.com>
2010-12-02 18:10:05 +0100

    [CondRobustness] New version of the web service

--- CondorcetRobustnessRelation/CondorcetRobustnessRelation.py
+++ CondorcetRobustnessRelation/CondorcetRobustnessRelation.py
@@ -7,15 +7,17 @@ import PyXMCDA
 
 from optparse import OptionParser
 
+VERSION = "1.2.1"
+
 
 ###
+
 
 def main(argv=None):
 	if argv is None:
 		argv = sys.argv
 	
 	parser = OptionParser()
-	
 	parser.add_option("-i", "--in", dest="in_dir")
 	parser.add_option("-o", "--out", dest="out_dir")
 	
@@ -27,220 +29,200 @@ def main(argv=None):
 	# Creating a list for error messages
 	errorList = []
 	
+	# If some mandatory folders haven't been provided
+	if not in_dir or not out_dir:
+		print "You have to provide an input and an output directory, using -i and -o command line options"
+		return
+	
+	if not os.path.isdir(out_dir):
+		print "The specified output directory doesn't exist"
+		return
+		
+	if not os.path.isdir(in_dir):
+		print "The specified input directory doesn't exist"
+		return
+		
 	# If some mandatory input files are missing
-	if not os.path.isfile (in_dir+"/alternatives.xml") or not os.path.isfile (in_dir+"/criteria.xml") or not os.path.isfile (in_dir+"/criteriaWeights.xml") or not os.path.isfile (in_dir+"/performanceTable.xml") :
+	if not os.path.isfile (in_dir+"/alternatives.xml") \
+	or not os.path.isfile (in_dir+"/criteria.xml") \
+	or not os.path.isfile (in_dir+"/criteriaWeights.xml") \
+	or not os.path.isfile (in_dir+"/performanceTable.xml") :
+		
 		errorList.append("Some input files are missing")
+		PyXMCDA.createMessagesFile (out_dir+"/messages.xml", [], [], errorList)
+		return
+		
+	# We parse all the mandatory input files
+	xmltree_alternatives = PyXMCDA.parseValidate(in_dir+"/alternatives.xml")
+	xmltree_criteria = PyXMCDA.parseValidate(in_dir+"/criteria.xml")
+	xmltree_weights = PyXMCDA.parseValidate(in_dir+"/criteriaWeights.xml")
+	xmltree_perfTable = PyXMCDA.parseValidate(in_dir+"/performanceTable.xml")
 	
-	else :
+	# We check if all madatory input files are valid
+	if xmltree_alternatives == None :
+		errorList.append("The alternatives file can't be validated.")
+	if xmltree_criteria == None :
+		errorList.append("The criteria file can't be validated.")
+	if xmltree_perfTable == None :
+		errorList.append("The performance table file can't be validated.")
+	if xmltree_weights == None :
+		errorList.append("The criteria weights file can't be validated.")
 		
-		# We parse all the mandatory input files
-		xmltree_alternatives = PyXMCDA.parseValidate(in_dir+"/alternatives.xml")
-		xmltree_criteria = PyXMCDA.parseValidate(in_dir+"/criteria.xml")
-		xmltree_weights = PyXMCDA.parseValidate(in_dir+"/criteriaWeights.xml")
-		xmltree_perfTable = PyXMCDA.parseValidate(in_dir+"/performanceTable.xml")
-		
-		# We check if all madatory input files are valid
-		if xmltree_alternatives == None :
-			errorList.append("The alternatives file can't be validated.")
-		if xmltree_criteria == None :
-			errorList.append("The criteria file can't be validated.")
-		if xmltree_perfTable == None :
-			errorList.append("The performance table file can't be validated.")
-		if xmltree_weights == None :
-			errorList.append("The criteria weights file can't be validated.")
-		
-	if not errorList :
+	if errorList:
+		PyXMCDA.createMessagesFile (out_dir+"/messages.xml", [], [], errorList)
+		return
 	
-		alternativesId = PyXMCDA.getAlternativesID(xmltree_alternatives)
-		criteriaId = PyXMCDA.getCriteriaID(xmltree_criteria)
-		perfTable = PyXMCDA.getPerformanceTable(xmltree_perfTable, alternativesId, criteriaId)
-		thresholds = PyXMCDA.getConstantThresholds (xmltree_criteria, criteriaId)
-		weights = PyXMCDA.getCriterionValue (xmltree_weights, criteriaId)
+	# We capture the data
+	alternativesId = PyXMCDA.getAlternativesID(xmltree_alternatives)
+	criteriaId = PyXMCDA.getCriteriaID(xmltree_criteria)
+	perfTable = PyXMCDA.getNumericPerformanceTable(xmltree_perfTable, alternativesId, criteriaId)
+	thresholds = PyXMCDA.getConstantThresholds (xmltree_criteria, criteriaId)
+	weights = PyXMCDA.getCriterionValue (xmltree_weights, criteriaId)
+	criteriaDir = PyXMCDA.getCriteriaPreferenceDirections (xmltree_criteria, criteriaId)
 		
-		if not alternativesId :
-			errorList.append("No alternatives found. Is your alternatives file correct ?")
-		if not criteriaId :
-			errorList.append("No criteria found. Is your criteria file correct ?")
-		if not perfTable :
-			errorList.append("No performance table found. Is your performance table file correct ?")
-		if not weights :
-			errorList.append("No weights found. Is your weights file correct ?")
+	if not alternativesId :
+		errorList.append("No alternatives found. Is your alternatives file correct ?")
+	if not criteriaId :
+		errorList.append("No criteria found. Is your criteria file correct ?")
+	if not perfTable :
+		errorList.append("No performance table found. Is your performance table file correct ?")
+	if not weights :
+		errorList.append("No weights found. Is your weights file correct ?")
 		
-	if not errorList :
+	if errorList:
+		PyXMCDA.createMessagesFile (out_dir+"/messages.xml", [], [], errorList)
+		return
 	
-		# We create the weight class (only considering the active criteria)
+	# We create the weight classes and the weight sum (only considering the active criteria)
+	uniqueWeights = []
+	classWeights = {}
+	sumWeights = 0.0
+	
+	for crit in criteriaId :
+		try :
+			sumWeights = sumWeights + weights[crit]
+			if classWeights.has_key (str(weights[crit])) :
+				classWeights[str(weights[crit])].append(crit)
+			else :
+				classWeights[str(weights[crit])] = [crit]
+				uniqueWeights.append(str(weights[crit]))
+		except :
+			errorList.append("There is no defined weight for criterion "+crit+".")
+			PyXMCDA.createMessagesFile (out_dir+"/messages.xml", [], [], errorList)
+			return
+	
+	uniqueWeights = sorted(uniqueWeights, reverse=True)
+	
+	# We transform the values for criteria to be minimized
+	for crit in criteriaId:
+		if criteriaDir.has_key (crit) and criteriaDir[crit] == "min" :
+			for alt in alternativesId:			
+				perfTable[alt][crit] = - perfTable[alt][crit]
+	
+	# We compute the alternative comparisons values
+	fileAltValues = open(out_dir+"/alternativesComparisons.xml", 'w')
+	PyXMCDA.writeHeader (fileAltValues)
+	
+	# We write some information about the generated file
+	fileAltValues.write ("\t<projectReference>\n\t\t<title>Condorcet robustness relation</title>\n\t\t<version>"+VERSION+"</version>\n\t\t<author>ws_PyXMCDA suite (TV)</author>\n\t</projectReference>\n\n")
+	
+	fileAltValues.write ("\t<alternativesComparisons mcdaConcept='CondorcetRobustnessRelation'>\n\t\t<pairs>\n")
+	
+	ElemOutranking = PyXMCDA.getRubisElementaryOutranking (alternativesId, criteriaId, perfTable, thresholds)
+	tabVeto = PyXMCDA.getVetos (alternativesId, criteriaId, perfTable, thresholds)
+	
+	for alt1 in alternativesId:
+		for alt2 in alternativesId:
 		
-		uniqueWeights = []
-		classWeights = {}
-		
-		for crit in criteriaId :
+			fileAltValues.write("\t\t\t<pair>\n\t\t\t\t<initial><alternativeID>"+alt1+"</alternativeID></initial>\n\t\t\t\t<terminal><alternativeID>"+alt2+"</alternativeID></terminal>\n")
+			
+			if tabVeto.has_key(alt1) and tabVeto[alt1].has_key(alt2) and not tabVeto[alt1][alt2] is None :
+				# Veto situation
+				fileAltValues.write ("\t\t\t\t<value><integer>-3</integer></value>\n\t\t\t</pair>\n")
+				continue
+			
+			ElementaryVector = ElemOutranking[alt1][alt2]
+			sum = 0.0
+			probComparisons = False
+			
 			try :
-				if classWeights.has_key (str(weights[crit])) :
-					classWeights[str(weights[crit])].append(crit)
+				for crit in criteriaId:
+					sum = sum + weights[crit]*ElemOutranking[alt1][alt2][crit]
+			except:
+				errorList.append("some alternatives evaluations are missing for criterion "+crit+", during the comparison between "+alt1+" and "+alt2+".")
+				probComparisons = True
+			
+			if probComparisons :
+				fileAltValues.write ("\t\t\t\t<value><NA>not available</NA></value>\n\t\t\t</pair>\n")
+				continue
+				
+			sum = sum/sumWeights
+			
+			# We compute the corresponding Condorcet denotation
+			if sum == 0 :
+				# m3 denotation
+				fileAltValues.write ("\t\t\t\t<value><integer>-3</integer></value>\n\t\t\t</pair>\n")
+			elif sum == 1 :
+				# p3 denotation
+				fileAltValues.write ("\t\t\t\t<value><integer>3</integer></value>\n\t\t\t</pair>\n")
+			elif sum == 0.5 :
+				# 0 denotation
+				fileAltValues.write ("\t\t\t\t<value><integer>0</integer></value>\n\t\t\t</pair>\n")
+			elif sum < 0.5 :
+				# m1 or m2 denotation
+				conditionm2Ok = True
+				strictInequality = False
+				sumPro = 0.0
+				sumAgainst = 0.0
+				
+				for we in uniqueWeights :
+					for crit in classWeights[we] :
+						sumPro += ElementaryVector[crit]
+						sumAgainst += 1.0 - ElementaryVector[crit]
+					if sumPro > sumAgainst :
+						conditionm2Ok = False
+						break
+					if sumPro < sumAgainst :
+						strictInequality = True
+				
+				if conditionm2Ok and strictInequality :
+					fileAltValues.write ("\t\t\t\t<value><integer>-2</integer></value>\n\t\t\t</pair>\n")
 				else :
-					classWeights[str(weights[crit])] = [crit]
-					uniqueWeights.append(str(weights[crit]))
-			except :
-				errorList.append("There is no defined weight for criterion "+crit+".")
-	
-		uniqueWeights = sorted(uniqueWeights, reverse=False)
-	
-	if not errorList :
-	
-		# We compute the weight sum (only the weights associated to active criteria)
-		sumWeights = 0.0
-		
-		for crit in criteriaId :
-			try :
-				sumWeights = sumWeights + weights[crit]
-			except :
-				errorList.append("There is no defined weight for criterion "+crit+".")
+					fileAltValues.write ("\t\t\t\t<value><integer>-1</integer></value>\n\t\t\t</pair>\n")
+			else :
+				# p1 or p2 denotation
+				conditionp2Ok = True
+				strictInequality = False
+				sumPro = 0.0
+				sumAgainst = 0.0
 				
-	if not errorList :
-		
-		# We recover the criteria preference directions
-		criteriaDir = PyXMCDA.getCriteriaPreferenceDirections (xmltree_criteria, criteriaId)
-		
-		# On retourne les valeurs pour les criteres a minimiser
-		# ATTENTION ! NE MARCHE QUE SI VALEUR NUMERIQUES !!!
-		for crit in criteriaId:
-			if criteriaDir.has_key (crit) and criteriaDir[crit] == "min" :
-				for alt in alternativesId:			
-					perfTable[alt][crit] = -perfTable[alt][crit]
-		
-		# We compute the alternative comparisons values
-		fileAltValues = open(out_dir+"/alternativesComparisons.xml", 'w')
-		PyXMCDA.writeHeader (fileAltValues)
-		
-		fileAltValues.write ("\t<alternativesComparisons mcdaConcept='CondorcetRobustnessRelation'>\n\t\t<pairs>\n")
-			
-		for alt1 in alternativesId: #["Flat 7"]: #alternativesId :
-			for alt2 in alternativesId: #["Flat 2"]: #alternativesId :
-			
-				fileAltValues.write("\t\t\t<pair>\n\t\t\t\t<initial><alternativeID>"+alt1+"</alternativeID></initial>\n\t\t\t\t<terminal><alternativeID>"+alt2+"</alternativeID></terminal>\n")
+				for we in uniqueWeights :
+					for crit in classWeights[we] :
+						sumPro += ElementaryVector[crit]
+						sumAgainst += 1.0 - ElementaryVector[crit]
+					if sumPro < sumAgainst :
+						conditionp2Ok = False
+						break
+					if sumPro > sumAgainst :
+						strictInequality = True
 				
-				ElementaryVector = {}
-				sum = 0.0
-				probComparisons = False
-				
-				for crit in criteriaId :
-				
-					# If the value of one criteria is NA
-					if perfTable[alt1][crit] == "NA" or perfTable[alt2][crit] == "NA":
-						print "NANANANANANANANAN"
-						
-					try :
-						if perfTable[alt1][crit] >= perfTable[alt2][crit] :
-							sum = sum + weights[crit]
-							ElementaryVector[crit] = 1
-						else :
-							if not thresholds[crit].has_key('indifference') and not thresholds[crit].has_key('preference') :
-								# No constant thresholds are defined for the seleccted criterion
-								ElementaryVector[crit] = 0
-							else :
-								if (thresholds[crit].has_key('indifference') != thresholds[crit].has_key('preference')) :
-									# An indifference Xor a preference threshold has been defined
-									if thresholds[crit].has_key('indifference') :
-										if perfTable[alt1][crit] + thresholds[crit]["indifference"] >= perfTable[alt2][crit] :
-											sum = sum + weights[crit]
-											ElementaryVector[crit] = 1
-										else :
-											ElementaryVector[crit] = 0
-									else :
-										if perfTable[alt1][crit] + thresholds[crit]["preference"] >= perfTable[alt2][crit] :
-											sum = sum + weights[crit]
-											ElementaryVector[crit] = 1
-										else :
-											ElementaryVector[crit] = 0
-								else :
-									# An indifference and a preference thresholds have been defined
-									if perfTable[alt1][crit] + thresholds[crit]["indifference"] >= perfTable[alt2][crit] :
-										sum = sum + weights[crit]
-										ElementaryVector[crit] = 1
-									elif perfTable[alt1][crit] + thresholds[crit]["preference"] >= perfTable[alt2][crit] :
-										sum = sum + weights[crit]/2.0
-										ElementaryVector[crit] = 0.5
-									else :
-										ElementaryVector[crit] = 0
-					except :
-						errorList.append("some alternatives evaluations are missing for criterion "+crit+", during the comparison between "+alt1+" and "+alt2+".")
-						probComparisons = True
-				
-				if not probComparisons :
-					sum = sum/sumWeights
-					
-					# We compute the corresponding Condorcet denotation
-					if sum == 0 :
-						# m3 denotation
-						fileAltValues.write ("\t\t\t\t<value><integer>-3</integer></value>\n\t\t\t</pair>\n")
-					elif sum == 1 :
-						# p3 denotation
-						fileAltValues.write ("\t\t\t\t<value><integer>3</integer></value>\n\t\t\t</pair>\n")
-					elif sum == 0.5 :
-						# 0 denotation
-						fileAltValues.write ("\t\t\t\t<value><integer>0</integer></value>\n\t\t\t</pair>\n")
-					elif sum < 0.5 :
-						# m1 or m2 denotation
-						conditionm2Ok = True
-						strictInequality = False
-						sumPro = 0.0
-						sumAgainst = 0.0
-						
-						for we in uniqueWeights :
-							for crit in classWeights[we] :
-								sumPro += ElementaryVector[crit]
-								sumAgainst += 1.0 - ElementaryVector[crit]
-							if sumPro > sumAgainst :
-								conditionm2Ok = False
-								break
-							if sumPro < sumAgainst :
-								strictInequality = True
-						
-						if conditionm2Ok and strictInequality :
-							fileAltValues.write ("\t\t\t\t<value><integer>-2</integer></value>\n\t\t\t</pair>\n")
-						else :
-							fileAltValues.write ("\t\t\t\t<value><integer>-1</integer></value>\n\t\t\t</pair>\n")
-					else :
-						# p1 or p2 denotation
-						conditionp2Ok = True
-						strictInequality = False
-						sumPro = 0.0
-						sumAgainst = 0.0
-						
-						for we in uniqueWeights :
-							for crit in classWeights[we] :
-								sumPro += ElementaryVector[crit]
-								sumAgainst += 1.0 - ElementaryVector[crit]
-							if sumPro < sumAgainst :
-								conditionp2Ok = False
-								break
-							if sumPro > sumAgainst :
-								strictInequality = True
-						
-						if conditionp2Ok and strictInequality :
-							fileAltValues.write ("\t\t\t\t<value><integer>2</integer></value>\n\t\t\t</pair>\n")
-						else :
-							fileAltValues.write ("\t\t\t\t<value><integer>1</integer></value>\n\t\t\t</pair>\n")
+				if conditionp2Ok and strictInequality :
+					fileAltValues.write ("\t\t\t\t<value><integer>2</integer></value>\n\t\t\t</pair>\n")
 				else :
-					fileAltValues.write ("\t\t\t\t<value><NA>not available</NA></value>\n\t\t\t</pair>\n")
-			
-		fileAltValues.write ("\t\t</pairs>\n\t</alternativesComparisons>\n")
-			
-		PyXMCDA.writeFooter(fileAltValues)
-		fileAltValues.close()
+					fileAltValues.write ("\t\t\t\t<value><integer>1</integer></value>\n\t\t\t</pair>\n")
+		
+	fileAltValues.write ("\t\t</pairs>\n\t</alternativesComparisons>\n")
+	
+	PyXMCDA.writeFooter(fileAltValues)
+	fileAltValues.close()
 	
 	# Creating log and error file, messages.xml
-	fileMessages = open(out_dir+"/messages.xml", 'w')
-	PyXMCDA.writeHeader (fileMessages)
-	
 	if not errorList :
-	
-		PyXMCDA.writeLogMessages (fileMessages, ["Execution ok"])
+		PyXMCDA.createMessagesFile (out_dir+"/messages.xml", ["Execution ok"], [], [])
 	else :
-		PyXMCDA.writeErrorMessages (fileMessages, errorList)
-		
-	PyXMCDA.writeFooter(fileMessages)
-	fileMessages.close()
+		PyXMCDA.createMessagesFile (out_dir+"/messages.xml", [], [], errorList)
+
+
 
 if __name__ == "__main__":
     sys.exit(main())
